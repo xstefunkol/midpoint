52ff032d1e5d6fe4ed8af92bc3967b516f3410ef
Viliam Repan <vilo.repan@evolveum.com>
2014-09-27 22:15:57 +0200

    midpoint theme cleanup

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/theme/MidPointTheme.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/theme/MidPointTheme.java
@@ -1,9 +1,16 @@
 package com.evolveum.midpoint.web.theme;
 
 import de.agilecoders.wicket.core.settings.ITheme;
+import de.agilecoders.wicket.less.LessResourceReference;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.request.Url;
 import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.UrlResourceReference;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author lazyman
@@ -18,7 +25,37 @@ public class MidPointTheme implements ITheme {
     @Override
     public void renderHead(IHeaderResponse response) {
         response.render(CssHeaderItem.forReference(
-                new PackageResourceReference(MidPointTheme.class, "bootstrap.css")));
+                new PackageResourceReference(MidPointTheme.class, "bootswatch/bootstrap.css")));
+
+        response.render(CssHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./css/easy-pie-chart/jquery.easypiechart.css"))));
+
+        // bootstrap select, https://github.com/silviomoreto/bootstrap-select/releases
+        response.render(CssHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./css/bootstrap-select/bootstrap-multiselect.css"))));
+        response.render(CssHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./css/bootstrap-select/bootstrap-select.css"))));
+
+        response.render(JavaScriptHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./js/bootstrap-select/bootstrap-multiselect.js"))));
+        response.render(JavaScriptHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./js/bootstrap-select/bootstrap-select.js"))));
+
+        // jquery easy pie chart
+        response.render(CssHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./css/easy-pie-chart/jquery.easypiechart.css"))));
+        response.render(JavaScriptHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./js/easy-pie-chart/jquery.easypiechart.js"))));
+
+        // jquery pnotify
+        response.render(CssHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./css/pnotify/jquery.pnotify.default.css"))));
+        response.render(JavaScriptHeaderItem.forReference(
+                new UrlResourceReference(Url.parse("./js/pnotify/jquery.pnotify.js"))));
+
+        // midpoint theme
+        response.render(CssHeaderItem.forReference(
+                new LessResourceReference(MidPointTheme.class, "MidPointTheme.less")));
     }
 
     @Override
